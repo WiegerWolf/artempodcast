fc7c43b32944abde8cea2dd9d10d82a290f35db4
Your Name <you@example.com>
2023-09-18 15:49:37 +0200

    Add bouncing effect to video clip

--- scripts/generate_video.py
+++ scripts/generate_video.py
@@ -21,6 +21,28 @@ def get_audio_file_from_md(episode_number):
         data = yaml.safe_load(front_matter)
         return data['audio']
 
+def bounce_animation(clip, screen_size):
+    w, h = clip.size
+    screen_w, screen_h = screen_size
+
+    # Define initial position and velocity
+    position = [screen_w / 2, screen_h / 2]
+    velocity = [2, 1.5]  # [vx, vy]
+
+    def update_position(t):
+        position[0] += velocity[0]
+        position[1] += velocity[1]
+
+        # Bounce off the edges of the screen
+        if position[0] <= w / 2 or position[0] >= screen_w - w / 2:
+            velocity[0] = -velocity[0]
+        if position[1] <= h / 2 or position[1] >= screen_h - h / 2:
+            velocity[1] = -velocity[1]
+
+        return position[0] - w / 2, position[1] - h / 2
+
+    return clip.fx(vfx.track_points, fn_pos=update_position, apply_to='mask')
+
 def generate_video(episode_number):
     audio_filename = get_audio_file_from_md(episode_number)
     audio_url = os.path.join(MEDIA_BASE_URL, audio_filename)
@@ -30,15 +52,20 @@ def generate_video(episode_number):
     # Load the audio file
     audio = AudioFileClip(local_audio_path)
     
-    # Load the image file and set its duration to the duration of the audio
-    img_clip = ImageClip(LOGO_PATH, duration=audio.duration)
-    
-    # Set the audio of the image clip to the audio file
-    final_clip = img_clip.set_audio(audio)
+    # Create a blank video clip of the desired size and set its duration to the duration of the audio
+    screen_size = (1920, 1080)
+    video = ColorClip(screen_size, color=(0, 0, 0), duration=audio.duration)
+
+    # Load the image and apply the bouncing effect
+    logo = ImageClip(LOGO_PATH).resize(height=200)
+    bouncing_logo = bounce_animation(logo, screen_size).set_duration(audio.duration)
+
+    # Overlay the bouncing logo on the video
+    final_clip = CompositeVideoClip([video, bouncing_logo]).set_audio(audio)
     
     # Write the result to a file
     output_file = local_audio_path.replace(".mp3", ".mp4")
-    final_clip.write_videofile(output_file, fps=24, codec='libx264', audio_codec='aac', threads=4)
+    final_clip.write_videofile(output_file, fps=24, codec='libx264', audio_codec='aac', threads=8)
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
